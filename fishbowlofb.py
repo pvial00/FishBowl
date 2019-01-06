49ee0754a2d4a7e2b153b5b27c988aad48297a6f
Karl Zander <pvial@Karls-MacBook-Pro.local>
2019-01-05 17:25:58 -0800

    OFB mode

---
+++ fishbowlofb.py
@@ -0,0 +1,170 @@
+class FishBowlOFB:
+    rounds = 10
+    blocklen = 10
+    shift = 3
+    def tonums(self, key):
+        k = []
+        for x in xrange(len(key)):
+            k.append(ord(key[x]) - 65)
+        return k
+    
+    def genRkeys(self, key, rounds, blocklen):
+        k = []
+        for x in xrange(len(key)):
+            k.append(ord(key[x]) - 65)
+        roundkeys = []
+        for r in xrange(rounds):
+            rk = [0] * blocklen
+            c = 0
+            for x in xrange(len(k)):
+                k[x] = (k[x] + k[x]) % 26
+            for x in xrange(blocklen):
+                rk[x] = (rk[x] + k[c]) % 26
+                c = (c + 1) % len(k)
+            roundkeys.append(rk)
+        return roundkeys
+
+    def gensbox(self, key, blocklen):
+        S = []
+        k = self.tonums(key)
+        klen = len(k)
+        j = 0
+        c = 0
+        for el in k:
+            j = (j + el) % 26
+        for x in xrange(blocklen):
+            box = range(26)
+            for y in xrange(26):
+                k[c % klen] = (k[c % klen] + k[(c + 1) % klen]) % 26
+                j = (j + k[c % klen]) % 26
+                box[c], box[j] = box[j], box[c]
+                c = (c + 1) % 26
+            S.append(box)
+        return S
+
+    def subblock(self, block, S, blocklen):
+        b = list(block)
+        for x in xrange(blocklen):
+            b[x] = S[x][b[x]]
+        return b
+
+    def rotate(self, block, r):
+        for x in xrange(r):
+            block.append(block.pop(0))
+        return block
+
+    def rotateback(self, block, r):
+        for x in xrange(r):
+            block.insert(0, block.pop())
+        return block
+
+    def roundenc(self, left, right, blocklen, r):
+        right = self.rotate(right, self.shift)
+        for x in xrange(blocklen):
+            right[x] = (right[x] + self.roundkeys[r][x]) % 26
+            left[x] = self.S[x][left[x]]
+            left[x] = (left[x] + right[x]) % 26
+            right[x] = (right[x] + left[x]) % 26
+        return right, left
+    
+    def rounddec(self, left, right, blocklen, r):
+        for x in reversed(xrange(blocklen)):
+            left[x] = (left[x] - right[x]) % 26
+            right[x] = (right[x] - left[x]) % 26
+            right[x] = self.S[x].index(right[x])
+            left[x] = (left[x] - self.roundkeys[r][x]) % 26
+        left = self.rotateback(left, self.shift)
+        return right, left
+
+    def tochars(self, nums):
+        for x in xrange(len(nums)):
+            nums[x] = chr(nums[x] + 65)
+        return "".join(nums)
+
+    def encrypt(self, secret, key, iv):
+        k = self.tonums(key)
+        IV = self.tonums(iv)
+        ctxt  = []
+        c = 0
+        klen = len(k)
+        blocklen = self.blocklen
+        blocks = (len(secret) / blocklen) / 2
+        extra = (blocklen * 2) - (len(secret) % (blocklen * 2))
+        if extra != 0:
+            blocks += 1
+        s1 = 0
+        e1 = blocklen * 2
+        self.S = self.gensbox(key, blocklen)
+        self.roundkeys = self.genRkeys(key, self.rounds, blocklen)
+        for x in xrange(len(self.roundkeys)):
+            self.subblock(self.roundkeys[x], self.S, blocklen)
+            
+        block1 = list(IV[:len(IV) / 2])
+        block2 = list(IV[len(IV) / 2:])
+        for x in range(blocks):
+            block = self.tonums(secret[s1:e1])
+            s1 += blocklen * 2
+            e1 += blocklen * 2
+            #for y in xrange(blocklen):
+            #    block1[y] = (block1[y] + previous_block[y]) % 26
+            #for y in xrange(blocklen):
+            #    block2[y] = (block2[y] + previous_block[y + blocklen]) % 26
+
+            for r in xrange(self.rounds):
+                block1, block2 = self.roundenc(block1, block2, blocklen, r)
+            #previous_block = list(block1)
+            #previous_block.extend(block2)
+            k = list(block1)
+            k.extend(block2)
+            if x == (blocks - 1):
+                blocklen = len(block)
+            for y in xrange(blocklen * 2):
+                block[y] = (block[y] + k[y]) % 26
+                
+            ctxt.append(self.tochars(block))
+        return "".join(ctxt)
+    
+    def decrypt(self, secret, key, iv):
+        k = self.tonums(key)
+        IV = self.tonums(iv)
+        ctxt  = []
+        c = 0
+        klen = len(k)
+        blocklen = self.blocklen
+        blocks = len(secret) / (blocklen * 2)
+        if blocks == 0:
+            blocks += 1
+        s1 = 0
+        e1 = blocklen * 2
+        self.S = self.gensbox(key, blocklen)
+        self.roundkeys = self.genRkeys(key, self.rounds, blocklen)
+        for x in xrange(len(self.roundkeys)):
+            self.subblock(self.roundkeys[x], self.S, blocklen)
+        block1 = list(IV[:len(IV) / 2])
+        block2 = list(IV[len(IV) / 2:])
+        for x in range(blocks):
+            block = self.tonums(secret[s1:e1])
+            s1 += blocklen * 2
+            e1 += blocklen * 2
+            for r in reversed(xrange(self.rounds)):
+                block1, block2 = self.rounddec(block1, block2, blocklen, r)
+            k = list(block1)
+            k.extend(block2)
+            if x == (blocks - 1):
+                blocklen = len(block)
+            for y in xrange(blocklen * 2):
+                block[y] = (block[y] - k[y]) % 26
+            ctxt.append(self.tochars(block))
+        return "".join(ctxt)
+
+class FishBowlKDF:
+    def kdf(self, password, keylen=26, iterations=10):
+        diff = keylen - len(password)
+        iv = "AAAAAAAAAAAAAAAAAAAA"
+        for x in xrange(diff):
+            password += "A"
+        fb = FishBowlOFB()
+        key = password
+        for i in xrange(iterations):
+            key = fb.encrypt(key[:keylen], key, iv)
+        return key
---
+++ scripts/fbofb.py
@@ -0,0 +1,68 @@
+from fishbowlofb import FishBowlOFB, FishBowlKDF
+import sys, select, getpass, os, time, getopt, re
+
+ivlen = 20
+keylen = 26
+
+def geniv(ivlen):
+    iv = []
+    while True:
+        byte = os.urandom(1)
+        if ord(byte) >= 0 and ord(byte) <= 25:
+            iv.append(chr(ord(byte) + 65))
+        if len(iv) == ivlen:
+            break
+    return "".join(iv)
+
+def sterilize(text):
+    text =  "".join(re.findall("[a-zA-Z]+", text)).upper()
+    return text
+
+try:
+    mode = sys.argv[1]
+except IndexError as ier:
+    print "Error: Did you forget encrypt/decrypt?"
+    sys.exit(1)
+
+input_filename = sys.argv[2]
+output_filename = sys.argv[3]
+
+try:
+    infile = open(input_filename, "r")
+except IOError as ier:
+    print "Input file not found."
+    sys.exit(1)
+
+try:
+    outfile = open(output_filename, "w")
+except IOError as ier:
+    print "Output file not found."
+    sys.exit(1)
+
+try:
+    key = sys.argv[4]
+except IndexError as ier:
+    key = getpass.getpass("Enter key: ")
+
+fb = FishBowlOFB()
+#key = FishBowlKDF().kdf(key, keylen)
+
+start = time.time()
+data = infile.read()
+infile.close()
+
+if mode == "encrypt":
+    iv = geniv(ivlen)
+    c = fb.encrypt(sterilize(data), key, iv)
+    outfile.write(iv+c)
+elif mode == "decrypt":
+    iv = data[:ivlen]
+    data = data[ivlen:]
+    plain_text = fb.decrypt(sterilize(data), key, iv)
+    outfile.write(plain_text)
+outfile.close()
+
+end = time.time() - start
+bps = len(data) / end
+sys.stdout.write("Completed in "+str(end)+" seconds\n")
+sys.stdout.write(str(bps)+" bytes per second.\n")
